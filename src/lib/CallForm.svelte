d35d16797fa8b6c3e4a1a7ce8b88581f744a0983
Max Machon <max@machon.family>
2022-08-03 14:32:29 -0400

    Changed formatting and look/feel of CallForm

--- src/lib/CallForm.svelte
+++ src/lib/CallForm.svelte
@@ -4,6 +4,7 @@
   import { db } from '../firebase';
 
   export let uid: string;
+
   let callerName: string    = '';
   let date: string          = '';
   let description: string   = '';
@@ -43,23 +44,48 @@
   <div class="field">
     <label for="call-number" class="label">Phone</label>
     <div class="control">
-      <input id="call-number" class="input" type="tel" placeholder="e.g (317) 867-5309" bind:value={phoneNumber}>
+      <input
+        bind:value={phoneNumber}
+        id="call-number"
+        class="input"
+        type="tel"
+        size="20"
+        minlength="9"
+        maxlength="20"
+        placeholder="e.g (317) 867-5309"
+        required>
     </div>
   </div>
 
-  <!-- Call Duration -->
-  <div class="field">
-    <label for="call-duration" class="label">Call Duration</label>
-    <div class="control">
-      <input id="call-duration" class="input" type="text" placeholder="e.g 15:00" bind:value={talkTime}>
-    </div>
-  </div>
+  <div class="columns">
 
-  <!-- Date -->
-  <div class="field">
-    <label for="call-date" class="label">Date</label>
-    <div class="control">
-      <input id="call-date" class="input" type="date" bind:value={date}>
+    <!-- Call Duration -->
+    <div class="column">
+      <div class="field">
+        <label for="call-duration" class="label">Call Duration</label>
+        <div class="control">
+          <input
+            bind:value={talkTime}
+            id="call-duration"
+            class="input"
+            type="text"
+            size="10"
+            minlength="2"
+            maxlength="6"
+            placeholder="e.g 15:00"
+            required>
+        </div>
+      </div>
+    </div>
+
+    <!-- Date -->
+    <div class="column">
+      <div class="field">
+        <label for="call-date" class="label">Date</label>
+        <div class="control">
+          <input id="call-date" class="input" type="date" bind:value={date} required>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -67,7 +93,16 @@
   <div class="field">
     <label for="call-name" class="label">Name</label>
     <div class="control">
-      <input id="call-name" class="input" type="text" placeholder={`e.g ${randomName}`} bind:value={callerName}>
+      <input
+        bind:value={callerName}
+        id="call-name"
+        class="input"
+        type="text"
+        size="50"
+        minlength="2"
+        maxlength="50"
+        placeholder={`e.g ${randomName}`}
+        required>
     </div>
   </div>
 
@@ -75,17 +110,25 @@
   <div class="field">
     <label for="call-description" class="label">Description</label>
     <div class="control">
-      <input id="call-description" class="input" type="text" placeholder="e.g Existing ticket 12345" bind:value={description}>
+      <input
+        bind:value={description}
+        id="call-description"
+        class="input"
+        type="text"
+        size="100"
+        maxlength="100"
+        placeholder="e.g Existing ticket 12345"
+        required>
     </div>
   </div>
 
   <!-- Submit or Cancel Buttons -->
   <div class="field is-grouped">
     <div class="control">
-      <input class="button is-link" type="submit" value="Add">
+      <input class="button is-link is-rounded" type="submit" value="Add">
     </div>
     <div class="control">
-      <input on:click={reset} class="button is-link is-light" type="reset" value="Reset">
+      <input on:click={reset} class="button is-link is-light is-rounded" type="reset" value="Reset">
     </div>
   </div>
 </form>
