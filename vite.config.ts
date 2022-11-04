67c7467ae0feec8b123f754ea9d18f3787b30456
Max Machon <max@machon.family>
2022-11-04 18:13:07 -0400

    Enable Vite chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    splitVendorChunkPlugin(),
+    svelte(),
+  ],
   envDir: '.',
   envPrefix: 'FIREBASE',
 });
