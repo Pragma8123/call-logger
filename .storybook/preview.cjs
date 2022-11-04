fa2b946245ef544ce7d4df5273326564bca542ad
Max Machon <max@machon.family>
2022-11-04 17:52:41 -0400

    Import Bulma base Sass modules in all stories

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,3 +1,6 @@
+// Import bulma base Sass for all components. Individual components should still import required Bulma Sass modules.
+import 'bulma/sass/base/_all.sass';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
