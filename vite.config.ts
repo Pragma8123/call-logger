3ac7d57d6cc7bd1c67c2d8f5cd7db0c1027a028e
Max Machon <max@machon.family>
2022-11-16 12:28:26 -0500

    style: fix styling issue in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    splitVendorChunkPlugin(),
-    svelte(),
-  ],
+  plugins: [splitVendorChunkPlugin(), svelte()],
   envDir: '.',
   envPrefix: 'FIREBASE',
 });
